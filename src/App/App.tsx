2241bc876fb04adc05beb03b3e8d9164dcd985c9
Luke Sontrop <luke@bluesaltlabs.com>
2023-04-15 21:37:20 -0700

    Installs tailwind and typescript.

---
+++ src/App/App.tsx
@@ -0,0 +1,14 @@
+import './App.scss'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Hello There. This is the bluesaltlabs.com home page.
+      </h1>
+    </div>
+  )
+}
+
+export default App
