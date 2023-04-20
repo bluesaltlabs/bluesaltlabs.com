c451eceffb84b920335ebaae58ed67cb2d272e97
Luke Sontrop <luke@bluesaltlabs.com>
2023-04-20 11:45:38 -0700

    Adds more cleanup in an attempt to get production to deploy.

--- src/Components/PageSection.jsx
+++ src/Components/PageSection.jsx
@@ -1,6 +1,6 @@
 
 
-export function PageSection({ className, children }) {
+function PageSection({ className, children }) {
   return (
     <section className={`bg-gray-100 dark:bg-gray-900 mb-4 rounded ${className ?? ''}`}>
       <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
