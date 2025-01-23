1d7076c2e7dbc06ab2d175d5d6274c8c396e3cdd
Luke Sontrop <luke@bluesaltlabs.com>
2025-01-23 15:34:17 -0800

    Renames invalid filename. Adds basic metronome.

---
+++ src/components/music/music-metronome.js
@@ -0,0 +1,60 @@
+import { LitElement, css, html } from "lit";
+
+export class MusicMetronome extends LitElement {
+  static get properties() {
+    return {
+      isPlaying: { type: Boolean },
+    };
+  }
+
+  constructor() {
+    super();
+    this.isPlaying = false;
+  }
+
+  render() {
+    return html`
+      <button @click=${this.togglePlay}>
+        ${this.isPlaying ? "Stop" : "Start"}
+      </button>
+    `;
+  }
+
+  togglePlay() {
+    this.isPlaying = !this.isPlaying;
+    if (this.isPlaying) {
+      this.start();
+    } else {
+      this.stop();
+    }
+  }
+
+  start() {
+    // todo
+    console.log("Music Metronome: start");
+  }
+
+  stop() {
+    // todo
+    console.log("Music Metronome: stop");
+  }
+
+
+
+  static get styles() {
+    return css`
+      button {
+        font-size: 1.5rem;
+        padding: 0.5rem 1rem;
+        border: none;
+        border-radius: 4px;
+        background-color: #007bff;
+        color: white;
+        cursor: pointer;
+      }
+    `;
+  }
+
+}
+
+customElements.define('music-metronome', MusicMetronome);
