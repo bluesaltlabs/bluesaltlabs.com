bca6e0f13d642e1d73e8f84004cd20e1c2b13fdd
Luke Sontrop <luke@bluesaltlabs.com>
2025-01-30 17:51:04 -0800

    Adds working vanilla js drag and drop demo.

--- src/components/ui/drag-drop.js
+++ src/components/ui/drag-drop.js
@@ -3,8 +3,9 @@ import { LitElement, html, css } from 'lit'
 // https://github.com/SortableJS/Sortable
 //import Sortable from 'sortablejs'
 
-// todo: look into this: https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API
-// todo: also look at this: https://www.horuskol.net/blog/2020-08-15/drag-and-drop-elements-on-touch-devices
+// https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API#examples
+// https://www.horuskol.net/blog/2020-08-15/drag-and-drop-elements-on-touch-devices
+// https://stackoverflow.com/a/55996629/5121100
 //
 //
 
@@ -20,12 +21,16 @@ class DragDropApp extends LitElement {
       flex-wrap: wrap;
     }
 
-    .drag-drop__container {
-      width:300px;
+    .drag-drop__container,
+    ::slotted(.drag-drop__container) {
+      /* width:300px; */
+      /* height: 300px; */
+      outline: 1px solid blue;
+      background-color: #ff441180;
 
-      border:1px solid #fff;
-      background-color:#ff4411;
-      padding: 10px;
+      padding: 1em;
+      height: 10em;
+      width: 50%;
     }
 
     .drag-drop__item,
@@ -35,9 +40,12 @@ class DragDropApp extends LitElement {
       border:1px solid #ffff00;
       background-color: #cc0011;
       cursor: move;
+      height: 100%;
+      width: 100%;
     }
 
 
+    /*
     .sortable-ghost,
     ::slotted(.sortable-ghost) {
       opacity: 0.4;
@@ -48,6 +56,7 @@ class DragDropApp extends LitElement {
     ::slotted(.sortable-drag) {
       background-color: #0000ff;
     }
+    */
   `
 
 
@@ -63,6 +72,8 @@ class DragDropApp extends LitElement {
 
   }
 
+
+
   //firstUpdated() {
     //const container = this.shadowRoot.querySelector('.drag-drop__container');
     //const container = this.querySelector('.drag-drop__container');
@@ -71,36 +82,88 @@ class DragDropApp extends LitElement {
    //}
 
   _pickup(event) {
+    this._logDebugEvent(event)
     this.moving = true
+
+
+    console.debug("before", {
+      height: event.target.style.height,
+      width: event.target.style.width,
+      position: event.target.style.position,
+    })
+
+    event.target.style.height = event.target.clientHeight
+    event.target.style.width = event.target.clientWidth
+    event.target.style.position = 'fixed'
+
+
+    console.debug("after", {
+      height: event.target.style.height,
+      width: event.target.style.width,
+      position: event.target.style.position,
+    })
   }
 
-  _handleTestEvent(event) {
-    console.debug("handling test event", { type: event.type, localName: event.target.localName })
+  _dragOver(event) {
+    event.preventDefault()
   }
+
+  _move(event) {
+    //this._logDebugEvent(event) // todo: throttle this or something D:
+    // note: I intentionally did not abstract things into variables here for the sake of efficiency.
+    //       I don't know if that matters or not in JS? that would be an interesting performance test.
+
+    // track movement
+    if (this.moving) {
+      if(event.clientX) {
+        // mousemove
+        console.debug({ x: event.clientX, y: event.clientY })
+        event.target.style.left = event.clientX - event.target.clientWidth/2;
+        event.target.style.top = event.clientY - event.target.clientHeight/2;
+      } else {
+        // touchmove - assuming a single touchpoint
+        console.debug({ x: event.event.changedTouches[0].clientX, y: event.event.changedTouches[0].clientY })
+        event.target.style.left = event.changedTouches[0].clientX - event.target.clientWidth/2;
+        event.target.style.top = event.changedTouches[0].clientY - event.target.clientHeight/2;
+      }
+    }
+  }
+
+  _drop(event) {
+    this._logDebugEvent(event)
+    this.moving = false
+  }
+
+  _logDebugEvent(event) {
+    console.debug("|------------- DEBUG -------------|",
+    { type: event.type, localName: event?.target?.localName })
+    console.debug("|---------------------------------|")
+  }
+
+
+  // todo: add a drag handle, or make it so dragging in certain places prevents drag.
 
   render() {
     return html`
       <div class="drag-drop__wrapper">
 
         <div class="drag-drop__container container-left">
-
-          <div class="drag-drop__left">
-            <div
-              @mousedown="${this._handleTestEvent}"
-              @touchstart="${this._handleTestEvent}"
-              class="drag-drop__item"
-            >
-              item 1
-            </div>
-            <!-- <div class="drag-drop__item">item 2</div> -->
+          <div
+            class="drag-drop__item"
+            @mousedown="${this._pickup}"
+            @touchstart="${this._pickup}"
+            @mousemove="${this._move}"
+            @touchmove="${this._move}"
+            @dragover="${this._dragOver}"
+            @mouseup="${this._drop}"
+            @touchend="${this._drop}"
+          >
+            Hi there
           </div>
+          <!-- <div class="drag-drop__item">item 2</div> -->
         </div>
 
-        <div class="drag-drop__container container-right">
-          <div class="drag-drop__right">
-            <!-- <div class="drag-drop__item">item 3</div> -->
-          </div>
-        </div>
+        <div class="drag-drop__container container-right"></div>
 
 
           <!-- <slot></slot> -->
