3b44abf6e8fbe7303e66a39140678495d2da78fd
Luke Sontrop <luke@bluesaltlabs.com>
2025-02-01 19:47:12 -0800

    Cleaning some things up.

---
+++ demos/spa/app.js
@@ -0,0 +1,8 @@
+// todo: see https://blog.jeremylikness.com/blog/build-a-spa-site-with-vanillajs/
+// also https://www.geeksforgeeks.org/javascript-fetch-method/
+
+const app = async () => {
+  // todo
+};
+
+document.addEventListener("DOMContentLoaded", app);
