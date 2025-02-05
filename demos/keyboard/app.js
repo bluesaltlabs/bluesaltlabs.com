b79489c9d39a06ac3e161266fb276024c88aafa7
Luke Sontrop <luke@bluesaltlabs.com>
2025-02-04 21:20:27 -0800

    Adds some work in progress cleanup to the keyboard.

--- demos/keyboard/app.js
+++ demos/keyboard/app.js
@@ -1,28 +1,39 @@
-// todo: convert this to a clas instead.
+
 
 /* Keyboard App */
 class KeyboardDemoApp {
   #foobar = "thing"
-  constructor() {
-    // public properties go here with
-    // this.foo = "bar" // syntax
+  constructor(octives = 1, firstOctive = 2) {
+    // Public Properties
+    this.octives    = octives;
+    this.firstOctive = firstOctive;
+
+    console.debug("KeyboardDemoApp settings: ", { octives: this.octives, firstOctive: this.firstOctive })
   }
 
   handleEvent(event) {
-    console.debug("KeyboardDemoApp is handling event", event)
+    console.debug("KeyboardDemoApp event handler:", event)
 
   }
 
   init() {
     document.addEventListener("DOMContentLoaded", (event) => {
+      this.handleEvent( new Event("app_load") )
       // todo: on page load
+      //
     });
   }
 }
 
+// keyboard features:
+// bind user keyboard (with a toggle switch) such that pressing keys plays the keyboard (like a normal DAW)
+//  - ability to turn this on and off
+// one octive is one section, create the keyboard as an SVG and duplicate per octive?
+//
+
 
 // Load Page App
-let kbApp = new KeyboardDemoApp();
+let kbApp = new KeyboardDemoApp(1, 4);
 kbApp.init()
 
 // -------------------------------------------------------------------------- //
