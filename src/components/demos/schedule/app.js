c25619501acb081cd9bdd3f59ff237a6fc05dafd
Luke Sontrop <luke@bluesaltlabs.com>
2025-02-07 11:52:59 -0800

    Adds ticker to page with Event API.

--- src/components/demos/schedule/app.js
+++ src/components/demos/schedule/app.js
@@ -1,9 +1,26 @@
 
+// Schedule App
+// This demo app uses the browser's Event API to register events and observers.
+// This page is an experiment in using native brower APIs instead of external libraries
+// for reactivity.
+//
+// 1. Set this.time to a new Date() object (so it's not null);
+// 2. Register Event Listeners
+//   - EVENT_TYPE__BUILD: fires once after constructor() completes
+//   - EVENT_TYPE__INIT: fires once after init() completes
+// 3. Register Intervals
+//   - INTERVAL_TICK
+
 const debug = true;
-const EVENT_TYPE__BUILD      = "build"; // constructor() complete
-const EVENT_TYPE__INIT       = "init";  // init() complete
-const EVENT_TYPE__TEST       = "test";  // test
-const EVENT_TYPE__TIMER_TICK = "timer_tick";  // timer tick
+const EVENT_TYPE__BUILD       = "build"; // constructor() complete
+const EVENT_TYPE__INIT        = "init";  // init() complete
+const EVENT_TYPE__TEST        = "test";  // test
+const EVENT_TYPE__TIMER_TICK  = "timer_tick";  // timer tick
+const INTERVAL_TICK           = "tick";
+const TICK_LENGTH             = 1000; // 1000 ms i.e. 1 second
+
+const EVENT_TYPE__UPDATE_CONTAINER  = "update_container";  // timer tick (temp)
+
 
 class ScheduleApp extends EventTarget {
   constructor() {
@@ -14,8 +31,16 @@ class ScheduleApp extends EventTarget {
       console.time(EVENT_TYPE__INIT);
     }
 
+    // init time values
+    //this.now = new Date();
+    this.seconds = 0;
+
+    // app container (temp?)
+    this.app_container = null;
+
     this._registerEventListeners();
-    this.dispatchEvent(this.events.build);
+    this._registerIntervals();
+    this.dispatchEvent(this.events.build); // EVENT_TYPE__BUILD
   }
 
   handleEvent(e) {
@@ -24,20 +49,13 @@ class ScheduleApp extends EventTarget {
     }
   }
 
-  _registerIntervals() {
-
-  }
-
-  _unregisterIntervals() {
-    // todo
-  }
-
   _registerEventListeners() {
     this.events = {
       build: new Event(EVENT_TYPE__BUILD),
       init: new Event(EVENT_TYPE__INIT),
       test: new Event(EVENT_TYPE__TEST),
-      timer_tick: new CustomEvent(EVENT_TYPE__TIMER_TICK, { tick: "foobar" }),
+      timer_tick: new Event(EVENT_TYPE__TIMER_TICK),
+      update_container: new Event(EVENT_TYPE__UPDATE_CONTAINER),
     }
 
     // build
@@ -58,16 +76,31 @@ class ScheduleApp extends EventTarget {
 
     // test
     this.addEventListener(EVENT_TYPE__TEST, (e) => {
+
       if(debug === true) {
         console.debug("handling 'test' event", { e: e ?? undefined });
       }
     });
 
+    this.addEventListener(EVENT_TYPE__UPDATE_CONTAINER, (e) => {
+      // todo: This is temporary!! use observer / subscribers instead!
+
+      //this.app_container.innerHTML = `Hello from the ScheduleApp class! ${this.now.toString()}`;
+      this.app_container.innerHTML = `Hello from the ScheduleApp class! (${this.seconds} seconds elapsed).`;
+    });
+
     // timer_tick
     this.addEventListener(EVENT_TYPE__TIMER_TICK, (e) => {
+      // Update value of now and seconds
+      //this.now = new Date();
+      this.seconds = this.seconds + 1;
+
       if(debug === true) {
-        console.debug("handling 'timer_tick' event", { e: e ?? undefined, tick: e?.tick ?? undefined });
+        //console.debug(`'tick': ${this.seconds} (${this.now.toString()})`);
+        //console.debug(`'tick': ${this.seconds}`);
       }
+
+      app.dispatchEvent(app.events.update_container); // EVENT_TYPE__UPDATE_CONTAINER
     });
   }
 
@@ -75,14 +108,23 @@ class ScheduleApp extends EventTarget {
     // todo
   }
 
+  _registerIntervals() {
+    this.intervals = {
+      tick: setInterval(() => { this.dispatchEvent(this.events.timer_tick) }, TICK_LENGTH),
+    }
+  }
+
+  _unregisterIntervals() {
+    // todo
+  }
+
   init() {
     document.addEventListener("DOMContentLoaded", (event) => {
-      const appContainer = document.getElementById('schedule-app');
+      //
+      this.app_container = document.getElementById('schedule-app');
 
-      // todo
-      appContainer.innerHTML = "Hello from the ScheduleApp class!";
-
-      this.dispatchEvent(this.events.init);
+      app.dispatchEvent(app.events.test);
+      this.dispatchEvent(this.events.init); // EVENT_TYPE__INIT
     });
   }
 };
@@ -91,5 +133,5 @@ class ScheduleApp extends EventTarget {
 let app = new ScheduleApp();
 app.init();
 
-app.dispatchEvent(app.events.test);
+
 //app.dispatchEvent(app.events.timer_tick);
