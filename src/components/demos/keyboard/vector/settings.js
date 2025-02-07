ead38593eb4d0a32d31d108dbb5b70074d3ff470
Luke Sontrop <luke@bluesaltlabs.com>
2025-02-07 00:43:39 -0800

    Updates colors.

--- src/components/demos/keyboard/vector/settings.js
+++ src/components/demos/keyboard/vector/settings.js
@@ -11,35 +11,35 @@ export default {
   width_1: 48,    // Pixel width value 1.          (default: 24)
   keyGap: 4,      // The pixel gap between keys.   (default: 2)
   background: {
-    color: "#339c1a",
+    color: "#339c1a80",
     describe: ".a green background.",
   },
   keys: {
     black_key_01: {
       fill: {
-        color: "#000000",
-        stroke: "#FFFFFF",
+        color: "#222222",
+        stroke: "#cccccc",
         describe: "A piano key shape drawn in black.",
       },
     },
     white_key_01: {
       fill: {
-        color: "#FFFFFF",
-        stroke: "#000000",
+        color: "#eeeeee",
+        stroke: "#444444",
         describe: "A piano key shape drawn in white.",
       },
     },
     white_key_02: {
       fill: {
-        color: "#FFFFFF",
-        stroke: "#000000",
+        color: "#eeeeee",
+        stroke: "#444444",
         describe: "A piano key shape drawn in white.",
       },
     },
     white_key_03: {
       fill: {
-        color: "#FFFFFF",
-        stroke: "#000000",
+        color: "#eeeeee",
+        stroke: "#444444",
         describe: "A piano key shape drawn in white.",
       },
     },
