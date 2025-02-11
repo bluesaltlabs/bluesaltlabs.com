da736d9d859cd12e50a45c987c06420ed44cd7e1
Luke Sontrop <luke@bluesaltlabs.com>
2025-02-11 14:26:59 -0800

    Attempts to fix vite config issues.

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+  plugins: [
+    //tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      '@data': resolve(__dirname, './data'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+});
