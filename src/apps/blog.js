4e2404c2b6658a2b65fe2d9ef01bbe6dab929f54
Luke Sontrop <luke@bluesaltlabs.com>
2025-02-12 11:50:47 -0800

    Cleans up blog, adds more pages.

--- src/apps/blog.js
+++ src/apps/blog.js
@@ -1,9 +1,23 @@
 import { marked } from 'marked';
 
-
 import posts from '@/data/posts.json';
 import post1 from '@/data/posts/1.md?raw';
 import post2 from '@/data/posts/2.md?raw';
+import post3 from '@/data/posts/3.md?raw';
+import post4 from '@/data/posts/4.md?raw';
+import post5 from '@/data/posts/5.md?raw';
+import post6 from '@/data/posts/6.md?raw';
+import post7 from '@/data/posts/7.md?raw';
+import post8 from '@/data/posts/8.md?raw';
+import post9 from '@/data/posts/9.md?raw';
+import post10 from '@/data/posts/10.md?raw';
+import post11 from '@/data/posts/11.md?raw';
+import post12 from '@/data/posts/12.md?raw';
+import post13 from '@/data/posts/13.md?raw';
+import post14 from '@/data/posts/14.md?raw';
+import post15 from '@/data/posts/15.md?raw';
+import post16 from '@/data/posts/16.md?raw';
+import post17 from '@/data/posts/17.md?raw';
 
 const getPostContentByID = (postID) => {
 
@@ -11,6 +25,22 @@ const getPostContentByID = (postID) => {
   switch (postID) {
     case 1: return post1; break;
     case 2: return post2; break;
+    case 3: return post3; break;
+    case 4: return post4; break;
+    case 5: return post5; break;
+    case 6: return post6; break;
+    case 7: return post7; break;
+    case 8: return post8; break;
+    case 9: return post9; break;
+    case 10: return post10; break;
+    case 11: return post11; break;
+    case 12: return post12; break;
+    case 13: return post13; break;
+    case 14: return post14; break;
+    case 15: return post15; break;
+    case 16: return post16; break;
+    case 17: return post17; break;
+    default: return `No Content!`
   }
 };
 
@@ -24,17 +54,8 @@ const getPostContentByID = (postID) => {
 /*                                                                            */
 /* # Application Description                                                  */
 /*                                                                            */
-/* ## Initialize, determine page.                                             */
-// ### List page
-//   load
-// ### Show page
-//   ...
-// Search page
-//   ...
-//
-/*                                                                            */
-/*                                                                            */
-/*                                                                            */
+/* ## Initializes loads posts.                                                */
+/*  todo                                                                      */
 /*                                                                            */
 /*                                                                            */
 /******************************************************************************/
@@ -83,28 +104,29 @@ export class BlogApp {
   }
 
 /* ************************************************************************** */
-  async __init() {
-    // Fetch posts
-    await this.fetchPosts();
 
-    const template = document.createElement('div');
+async __init() {
+  // Fetch posts
+  await this.fetchPosts();
 
-    // todo: get post content for each post
-    // todo: error checking if no posts are retrieved.
-    this.posts.sort((a, b) => (b.id - a.id))
-      .filter((p) => (!!p.publishedAt))
-      .map((post, postKey, posts) => {
-        const postContentArticle = document.createElement('article');
-        const postContent = marked.parse(getPostContentByID(post.id));
+  const template = document.createElement('div');
 
-        postContentArticle.innerHTML = `
-          <h2 class="post-title" id="post-title_${post.id}">${post.title}</h2>
-          <div id="post-content_${post.id}>${postContent}</div>
-          ${postKey < posts.length - 1 ? '<hr />' : '' }
-        `;
+  // todo: get post content for each post
+  // todo: error checking if no posts are retrieved.
+  this.posts.sort((a, b) => (b.id - a.id))
+    .filter((p) => (!!p.publishedAt))
+    .map((post, postKey, posts) => {
+      const postContentArticle = document.createElement('article');
+      const postContent = marked.parse(getPostContentByID(post.id));
 
-        template.appendChild(postContentArticle)
-      });
+      postContentArticle.innerHTML = `
+        <h2 class="post-title" id="post-title_${post.id}">${post.title}</h2>
+        <div id="post-content_${post.id}">${postContent}</div>
+        ${postKey < (posts.length - 1) ? '<hr />' : '' }
+      `;
+
+      template.appendChild(postContentArticle)
+    });
 
     try {
       const mountPoint = document.getElementById(this.app.mountPointID);
@@ -121,8 +143,6 @@ export class BlogApp {
 
   load(mountPointID) {
     this.loadAppState(mountPointID);
-
-    // todo: any other tasks to complete before mounting DOM.
 
     // Mount elements to the page once the DOM Content has been loaded.
     document.addEventListener("DOMContentLoaded", (e) => { this.__init(); }, { once: true });
