3db9c660f7d52c9df240ae9d867197e7e1cb5587
Luke Sontrop <lukerobertsontrop@gmail.com>
2025-02-13 17:29:30 -0800

    Adds test files.

---
+++ data/test.md
@@ -0,0 +1,7 @@
+# Test Markdown File
+
+This is a test markdown file in the data directory.
+
+1. it is supposed to test
+2. how GitHub
+3. Returns this data when deployed.
--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
 
   ],
   //assetsInclude: ['**/*.md', '**/*.json'],
-  assetsInclude: ['**/*.md'],
+  assetsInclude: ['**/*.md', '/data/*'],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
