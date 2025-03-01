591f82def024a13d6fb8622fef53da80c93566dc
Luke Sontrop <lukerobertsontrop@gmail.com>
2025-03-01 11:32:42 -0800

    Adds route to sequencer notes page.

--- vite.config.js
+++ vite.config.js
@@ -25,6 +25,7 @@ export default defineConfig({
         demos: resolve(__dirname, 'demos/index.html'),
         demos_keyboard: resolve(__dirname, 'demos/keyboard/index.html'),
         demos_sequencer: resolve(__dirname, 'demos/sequencer/index.html'),
+        demos_sequencer_notes: resolve(__dirname, 'demos/sequencer/notes.html'),
         demos_controls: resolve(__dirname, 'demos/controls/index.html'),
         demos_vectors: resolve(__dirname, 'demos/vectors/index.html'),
         demos_vectors_notes: resolve(__dirname, 'demos/vectors/notes.html'),
