c78cd77fc0f76bee0a0af1b49316bdd87c919fb1
Luke Sontrop <lukerobertsontrop@gmail.com>
2025-03-01 10:52:13 -0800

    Installs some plugins for marked.

--- src/components/tools/markdown-parser.js
+++ src/components/tools/markdown-parser.js
@@ -1,10 +1,18 @@
-import { marked } from 'marked';
+import { marked } from "marked";
+import customHeadingId from "marked-custom-heading-id";
+import { gfmHeadingId } from "marked-gfm-heading-id";
+import markedCodeFormat from "marked-code-format";
 
 class MarkdownParser extends HTMLElement {
   constructor() { super() }
 
   connectedCallback() {
     try {
+      // Set up marked plugins
+      marked.use(customHeadingId());
+      marked.use(gfmHeadingId({ prefix: "" }));
+      marked.use(markedCodeFormat({ }));
+
       this.innerHTML = marked(`${this.textContent}`.trim());
     } catch (e) { console.error("MarkdownParser error", e); }
   }
